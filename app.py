ba0cbb65de9f62b7d372e6cdd80ec795d9875a7e
shady-15 <156305213+shady-15@users.noreply.github.com>
2024-01-12 00:38:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def get_game_data():
     yesterday = (datetime.now(us_timezone) - timedelta(days=1)).strftime('%Y-%m-%d')
     today = (datetime.now(us_timezone)).strftime('%Y-%m-%d')
 
-    # API endpoint URL parameters
-    games_url = f'https://www.balldontlie.io/api/v1/games?start_date={yesterday}&end_date={today}&per_page={500}'
+    # API endpoint URL with correct query parameters
+    games_url = f'https://www.balldontlie.io/api/v1/games?start_date={yesterday}&end_date={today}&per_page={100}'
 
-    # recup data
+    # Fetch data from the API
     response = requests.get(games_url)
     return response.json()['data']
 
@@ -25,20 +25,30 @@ def get_game_data():
 def get_game_stats():
     yesterday = (datetime.now(us_timezone) - timedelta(days=1)).strftime('%Y-%m-%d')
     today = (datetime.now(us_timezone)).strftime('%Y-%m-%d')
-    stats_url = f"https://www.balldontlie.io/api/v1/stats?start_date={yesterday}&end_date={today}&per_page={500}"
+    
+    stats_url = f"https://www.balldontlie.io/api/v1/stats?start_date={yesterday}&end_date={today}&per_page={100}"
     stats_response = requests.get(stats_url)
-    return stats_response.json()["data"]
+    
+    
+    stats = []
+    stats.extend(stats_response.json()["data"])
+    page = 2
+    while stats_response.json()['meta']['current_page'] <= stats_response.json()['meta']['total_pages']:
+        stats_url = f"https://www.balldontlie.io/api/v1/stats?start_date={yesterday}&end_date={today}&per_page={100}&page={page}"
+        stats_response = requests.get(stats_url)
+        stats.extend(stats_response.json()["data"])
+        
+        print(f"fetching page {page}", "len data", len(stats_response.json()["data"]))
+        page += 1
+    return stats
 
 
 def get_result(game_data, game_stats):
     
     basketball_games = {'today': [], 'yesterday': []}
     
-    # key game id
-    iter = 0
-    
+    # key is game id
     for game in game_data:
-        iter += 1
         game_id = game['id']
         
         top_performer = None
@@ -46,31 +56,39 @@ def get_result(game_data, game_stats):
         for game_state in game_stats:
             s_game_id = game_state['game']['id']
             if (game_id) == (s_game_id):
-                if game_state['player']== None : continue
-                player_name = game_state['player']['first_name'] + " " + game_state['player']['last_name']
-                    
-                pts = game_state['pts']
-                ast = game_state['ast']
-                reb = game_state['reb']
-                if top_performer == None or pts > top_performer[1]:
-                    top_performer = (player_name, pts, ast, reb)
+                if game_state['player']:
+                    player_name = game_state['player']['first_name'] + " " + game_state['player']['last_name']
+                        
+                    pts = game_state['pts']
+                    ast = game_state['ast']
+                    reb = game_state['reb']
+                    if top_performer == None or pts > top_performer[1]:
+                        top_performer = (player_name, pts, ast, reb)
                     
         game_day = datetime.strptime(game['date'], "%Y-%m-%dT%H:%M:%S.%fZ").day
         if game['home_team_score'] == game['visitor_team_score'] == 0:
             notification_message = (
-                        "\n" + "=" * 38 + "\n"
-                        f" **This game will be played later!**\n"
+                        "\n" + "=" * 70 + "\n"
+                        f" **This game will be played soon!**\n"
                         f":basketball: **{game['home_team']['full_name']}**\n"
                         f":basketball: **{game['visitor_team']['full_name']} **\n"
-                        "\n" + "=" * 38 + "\n"
+                        "\n" + "=" * 70 + "\n"
                     )
         elif top_performer:
             notification_message = (
-                        "\n" + "=" * 38 + "\n"
+                        "\n" + "=" * 70 + "\n"
                         f":basketball: **{game['home_team']['full_name']} {game['home_team_score']}**\n"
                         f":basketball: **{game['visitor_team']['full_name']} {game['visitor_team_score']}**\n"
                         f":trophy: ** {top_performer[0]}, {top_performer[1]}pts / {top_performer[2]} ast / {top_performer[3]} reb**\n"
-                        "\n" + "=" * 38 + "\n"
+                        "\n" + "=" * 70 + "\n"
+                    )
+        else:
+            notification_message = (
+                        "\n" + "=" * 70 + "\n"
+                        f" **Top scorer data unavailable now.**\n"
+                        f":basketball: **{game['home_team']['full_name']} {game['home_team_score']}**\n"
+                        f":basketball: **{game['visitor_team']['full_name']} {game['visitor_team_score']}**\n"
+                        "\n" + "=" * 70 + "\n"
                     )
 
             
@@ -80,20 +98,20 @@ def get_result(game_data, game_stats):
             basketball_games['yesterday'].append(notification_message)
     return basketball_games
         
-        
     
+
     
 @app.route("/send")
 def send():
     
-    game_data = get_game_data()  #game data yesterday and today
+    game_data = get_game_data()  # we get the game data for yesterday and today
     print("getting data")
     
-    game_stats =  get_game_stats()  #stats yesterday and today
+    game_stats =  get_game_stats()  # we will get the stats for yesterday and today
     print("getting state")
     
     basketball_games = get_result(game_data, game_stats)
-    print("getting basket ball")
+    print("getting basketball")
     
     webhook_url = 'https://discord.com/api/webhooks/1194944310852993075/STEHCw3REgL8QCUENXMQO79hea-4ScYy8Kv0xzJ94fl9fvw76UeH8TBLzyydn6bipXqV'
     # Calculate yesterday, today
@@ -123,8 +141,6 @@ def send():
     print("Done")
     return 'Message sent to discord Posting channel!'
     
-
-
 
 
 
